55e14c6ab536ec9da7651392d92c02767e7bdf5f
Ngan Pham <nganpham@gmail.com>
2013-12-05 22:49:40 -0800

    Fix some query methods, bump version

--- lib/rails_soft_deletable/query.rb
+++ lib/rails_soft_deletable/query.rb
@@ -4,27 +4,26 @@ module RailsSoftDeletable
   module Query
     extend ActiveSupport::Concern
 
-    included do
+    module ClassMethods
+      def soft_deletable?
+        true
+      end
+
+      def with_deleted
+        scoped.tap { |x| x.default_scoped = false }
+      end
+
+      def only_deleted
+        with_deleted.where("#{self.table_name}.#{soft_deletable_column} > 0")
+      end
       alias :deleted :only_deleted
-    end
 
-    def soft_deletable?
-      true
-    end
-
-    def with_deleted
-      scoped.tap { |x| x.default_scoped = false }
-    end
-
-    def only_deleted
-      with_deleted.where("#{self.table_name}.#{soft_deletable_column} > 0")
-    end
-
-    def restore(id)
-      if id.is_a?(Array)
-        id.map { |one_id| restore(one_id) }
-      else
-        only_deleted.find(id).restore!
+      def restore(id)
+        if id.is_a?(Array)
+          id.map { |one_id| restore(one_id) }
+        else
+          only_deleted.find(id).restore!
+        end
       end
     end
   end
